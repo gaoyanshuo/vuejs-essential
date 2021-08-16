28f01cbd25d324df5f4700e788871b3f117af6e9
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-08-16 21:35:47 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
+<!--<template lang="pug">-->
+<!--  div#app-->
+<!--    img(src="./assets/logo.png")-->
+<!--    h1 Welcome from pug & sass-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'App'-->
+<!--}-->
+<!--</script>-->
+
+<!--<style lang="scss">-->
+<!--#app {-->
+<!--  font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 200px;-->
+
+<!--  h1 {-->
+<!--    color: #42b983;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <h1>Welcome to LearnKu.com</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 200px;
 }
 </style>
